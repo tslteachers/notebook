08eadbae31e5504e2ccec4e4f9534487b42fd29c
Jeff Cohen <jeff@starterleague.com>
2013-08-05 09:30:33 -0500

    Start the app

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,46 @@
+class NotesController < ApplicationController
+
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find_by_id(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new
+    @note.description = params[:description]
+
+    if @note.save
+      redirect_to notes_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @note = Note.find_by_id(params[:id])
+  end
+
+  def update
+    @note = Note.find_by_id(params[:id])
+    @note.description = params[:description]
+
+    if @note.save
+      redirect_to notes_url
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @note = Note.find_by_id(params[:id])
+    @note.destroy
+    redirect_to notes_url
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo::Application.config.secret_key_base = '0cd58a03ef3cb715c169c99042d0fd4424e3e646914add97b6ecde0c10683cdb731557105aa7d043f6d431f05dec9b01802fe0e038933fc90bd4d1b8c977e009'
