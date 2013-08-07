fe403d20751a077190640a3147411584c39b2eeb
Jeff Cohen <jeff@starterleague.com>
2013-08-07 08:33:49 -0500

    Uh oh, the app is broken. Can you fix it?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # Parameters: {"utf8"=>"✓", "authenticity_token"=>"p8tdwgPIDQH0x4iQUmyO5fee6NTD1v3e0IyW2/UW8xE=", "name"=>"Jeff", "password"=>"[FILTERED]", "commit"=>"Let Me In"}
-
-    # user = User.find_by_name(params["name"])
     user = User.find_by :name => params["name"]
     if user
       if user.authenticate(params["password"])
-        # We're signed in!
-        session[:login_id] = user.id
         redirect_to root_url
       else
         redirect_to sign_in_url, notice: "Bad username or password"
